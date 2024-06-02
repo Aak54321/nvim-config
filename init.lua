57572cb3829fcb10b0383e7d36d9b4ea43f6e169
Aakash Shalini <saakash2k4@gmail.com>
2024-06-02 11:31:40 +0530

    attach all config files

---
+++ init.lua
@@ -0,0 +1,20 @@
+-- installing lazy package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+--importing my vim global vim key binds
+require("vim-binds")
+--importing all the necessary plugins from a module named "plugin"
+require("lazy").setup("plugin")
+
+
+
